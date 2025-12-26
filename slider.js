e1f42e8d477521a06c5f045684fc36f20903041c
Lincoln Araujo <lincoln.araujo@ebc.com.br>
2025-12-26 16:31:49 -0300

    atualização do carrossel

--- slider.js
+++ slider.js
@@ -1,19 +1,29 @@
-new Swiper(".mpf-swiper", {
-    slidesPerView: 1,
-    spaceBetween: 16,
-    loop: false,
-    speed: 450,
-    a11y: { enabled: true },
+const swiper = new Swiper(".mpf-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  loop: true,
+  speed: 700,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  a11y: { enabled: true },
+  pagination: {
+    el: ".mpf-swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".mpf-swiper-next",
+    prevEl: ".mpf-swiper-prev",
+  },
+});
 
-    // autoplay opcional (se quiser, descomenta)
-    // autoplay: { delay: 4500, disableOnInteraction: false },
+const swiperEl = document.querySelector(".mpf-swiper");
 
-    pagination: {
-      el: ".mpf-swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".mpf-swiper-next",
-      prevEl: ".mpf-swiper-prev",
-    },
-  });
+// pausa com mouse
+swiperEl.addEventListener("mouseenter", () => swiper.autoplay.stop());
+swiperEl.addEventListener("mouseleave", () => swiper.autoplay.start());
+
+// pausa com teclado (acessibilidade)
+swiperEl.addEventListener("focusin", () => swiper.autoplay.stop());
+swiperEl.addEventListener("focusout", () => swiper.autoplay.start());
